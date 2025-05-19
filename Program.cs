81090ebf4f6bfb3dfd93aecc2dbc9f0cb29dd770
ujjal_roy <ujjalroy7862@gmail.com>
2025-05-19 16:54:40 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Ujjal, whats up!");
+Console.WriteLine("Hello Ujjal, how are you doing today?");
+Console.WriteLine("Hello Ujjal, how is your family?");
