7bd8058625c66e161789b3fd337e9088503aa3a3
ujjal_roy <ujjalroy7862@gmail.com>
2025-05-20 17:35:05 +0600

    Update Program.cs to print an additional message and modify AssemblyInfo for new informational version

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿class Test
+﻿// This is a simple C# program that prints "Hello, World!" to the console.
+using System;
+
+class Test
 {
   public static void Main(string[] args)
   {
     Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World2");
   }
 }
